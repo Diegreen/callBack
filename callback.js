323834cfdc267597eb9293f7b4f35972f0cfcdc2
diego <diegonegocios21@outlook.com>
2023-06-29 01:26:26 -0300

    Add

--- callback.js
+++ callback.js
@@ -9,3 +9,20 @@ function imprimirResultado(resultado) {
     console.log("O resultado da soma é: " + resultado)
 }
 
+
+const letters = ["a", "b", "c", "d"];
+
+//letters.array.forEach(element => {
+    console.log(element)
+//});
+
+function forEach(array, callBackParaRodarEmCadaItem) {
+    for(let index = 0; index < array.length; index ++) {
+        const letraAtual = array[index]
+        callBackParaRodarEmCadaItem(letraAtual, index, array)
+    }
+}
+
+forEach(letters, function(letraAtual, index, array) {
+console.log(letraAtual)
+}) 
